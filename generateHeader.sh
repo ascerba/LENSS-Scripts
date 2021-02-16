9c36fd9f8dcdf3c5cf2070279e29b4adba815e56
Alex Scerba <a.scerba02@gmail.com>
2021-02-16 01:00:59 -0600

    Added some input validation

--- generateHeader.sh
+++ generateHeader.sh
@@ -15,30 +15,80 @@ echo "# Community Standard Skyglow Data Format 1.0
 # Instrument ID: $sensorName" > $headerFile
 
 read -p "Enter name(s) of the data providers / affiliated institution: " dataSupplier
+while ! [[ "$dataSupplier" =~ ^[a-zA-Z0-9_-]+$ ]]
+do
+	read -p "Error: Field can only contain alphanumeric characters, '-', and '_'. Enter a valid entry: " dataSupplier
+done
 echo "# Data supplier: $dataSupplier" >> $headerFile
 
 read -p "Enter the location as Country-State-City: " location
+while ! [[ "$location" =~ ^[a-zA-Z0-9_-]+$ ]]
+do
+	read -p "Error: Field can only contain alphanumeric characters, '-', and '_'. Enter a valid entry: " location
+done
 echo "# Location name: $location" >> $headerFile
 
 echo "--Enter exact position of sensor--"
 read -p "Longitude (four decimals): " lon
+while ! [[ "$lon" =~ ^[0-9-+].[0-9]{4}+$ ]]
+do
+	read -p "Error: Field must be formatted as a float to four decimal places. Enter a valid value: " lon
+done
 read -p "Latitude (four decimals): " lat
+while ! [[ "$lat" =~ ^[0-9-+].[0-9]{4}+$ ]]
+do
+	read -p "Error: Field must be formatted as a float to four decimal places. Enter a valid value: " lat
+done
 read -p "Elevation in meters (whole number): " elev
+while ! [[ "$elev" =~ ^[0-9]+$ ]]
+do
+	read -p "Error: Field must be formatted as a non-negative whole number. Enter a valid value: " elev
+done
 echo "# Position (lat, lon, elev(m)): $lon, $lat, $elev" >> $headerFile
 
-read -p "Enter local timezone of the sensor's location as Country/City (EX: America/Chicago): " timezone
+read -p "Enter local timezone of the sensor's location as Country/Region (EX: America/Chicago): " timezone
+while ! [[ "$timezone" =~ ^[a-zA-Z]/[a-zA-Z]+$ ]]
+do
+	read -p "Error: Timezone must be formatted as Country/Region. Enter a valid value: " timezone
+done
 echo "# Local timezone: $timezone" >> $headerFile
 
 read -p "Enter the time synchonization method for the sensor (EX: GPS): " timeSync
+while ! [[ "$timeSync" =~ ^[a-zA-Z]+$ ]]
+do
+	read -p "Error: Time sync method must be formatted as a single word or acronym. Enter a valid value: " timeSync
+done
 echo "# Time Synchronization: $timeSync" >> $headerFile
 
-read -p "Is the sensor moving or stationary (MOVING / STATIONARY): " positionMethod
-echo "# Moving / Stationary position: $positionMethod" >> $headerFile
+read -p "Is the sensor moving or stationary [ M / S ]: " positionMethod
+while ! [[ "$positionMethod" =~ ^[mMsS]{1}+$ ]]
+do
+	read -p "Error: Entry is invalid. Enter and M or S: " positionMethod
+done
+if [[ "$positionMethod" =~ ^[sS]{1}+$ ]]
+then
+	echo "# Moving / Stationary position: STATIONARY" >> $headerFile
+else if [[ "$positionMethod" =~ ^[mM]{1}+$ ]]
+	echo "# Moving / Stationary position: MOVING" >> $headerFile
+fi
 
 read -p "Is the sensor's look direction moving or fixed (MOVING / FIXED): " lookMethod
-echo "# Moving / Fixed look direction: $lookMethod" >> $headerFile
+while ! [[ "$lookMethod" =~ ^[mMfF]+$ ]]
+do
+	read -p "Error: Entry is invalid. Enter and M or F: " lookMethod
+done
+if [[ "$lookMethod" =~ ^[fF]{1}+$ ]]
+then
+	echo "# Moving / Stationary position: FIXED" >> $headerFile
+else if [[ "$lookMethod" =~ ^[mM]{1}+$ ]]
+	echo "# Moving / Stationary position: MOVING" >> $headerFile
+fi
 
 read -p "Enter the number of viewing channels on the sensor (typically 1): " numChannels
+while ! [[ "$numChannels" =~ ^[0-9]+$ ]] && [[ "$numChannels" == 0 ]]
+do
+	read -p "Error: Invalid number. Enter a non-zero, positive integer: " numChannels
+done
 echo "# Number of channels: $numChannels" >> $headerFile
 
 echo "--Enter the type of filter on each channel--"
@@ -46,6 +96,10 @@ filterList=""
 for ((i = 0; i < $numChannels; i++)); do
 	printf "Channel %d\n" $(( i + 1 ))
 	read -p "Enter filter type: " filter
+	while ! [[ "$filter" =~ ^[a-zA-Z0-9-_]+$ ]]
+	do
+		read -p "Error: Filter type can only contain alphanumeric characters and '-' and '_'. Re-enter: " filter
+	done
 	if [[ $i == $(( numChannels - 1 )) ]]; then
 		filterList="$filterList$filter"
 	else
@@ -59,7 +113,15 @@ directionList=""
 for ((i = 0; i < $numChannels; i++)); do
 	printf "Channel %d\n" $(( i + 1 ))
 	read -p "Enter northern angle: " northAngle
+	while ! [[ "$northAngle" =~ ^[0-9-].[0-9]{2}+$ ]]
+	do
+		read -p "Error: Number must be a signed float to two decimal places. Re-enter: " northAngle
+	done
 	read -P "Enter eastern angle: " eastAngle
+	while ! [[ "$eastAngle" =~ ^[0-9-].[0-9]{2}+$ ]]
+	do
+		read -p "Error: Number must be a signed float to two decimal places. Re-enter: " eastAngle
+	done
 	if [[ $i == $(( numChannels - 1 )) ]]; then
 		directionList="$directionList$northAngle, $eastAngle"
 	else
@@ -73,6 +135,10 @@ fieldsOfView=""
 for ((i = 0; i < $numChannels; i++)); do
 	printf "Channel %d\n" $(( i + 1 ))
 	read -p "Enter field of view: " fov
+	while ! [[ "$fov" =~ ^[0-9]+$ ]]
+	do
+		read -p "Error: Number must be an unsigned whole number greater than zero. Re-enter: " fov
+	done
 	if [[ $i == $(( numChannels - 1 )) ]]; then
 		fieldsOfView="$fieldsOfView$fov"
 	else
